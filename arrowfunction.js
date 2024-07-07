e45c297a51472c73556a6a0d7826f5836f3f3883
Muthulakshmisubi <161447612+Muthulakshmisubi@users.noreply.github.com>
2024-07-07 18:41:21 +0530

    Add files via upload

---
+++ arrowfunction.js
@@ -0,0 +1,5 @@
+let addition=(sum1,sum2)=>sum1+sum2;
+let a=10;
+let b=20;
+let sum=addition(a,b)
+console.log(sum)
---
+++ function.js
@@ -0,0 +1,8 @@
+let a=10;
+let b=20;
+let sum=additionFunction(a,b)
+console.log(sum)
+
+function additionFunction(sum1,sum2){
+    return a+b
+}
